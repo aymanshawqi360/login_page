747c20786d21d64f2cb97f4cee8812ee31072c20
aymanshawqi360 <aymanshawqi380gmail.com>
2024-11-27 23:02:55 +0300

    - added spacing file for SizedBox. - added some colors & TextStyle. - Finished  LoginScreen. - added general text_button & general text_form_field. - added in folder login text_terms_and_conditions & text_sing_up.

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -5,7 +5,7 @@ import 'package:login_app/core/theming/font_weight_helper.dart';
 
 class TextStyles {
   static TextStyle font24Black = TextStyle(
-    color: ColorManager.darkGray,
+    color: ColorManager.darkBlue,
     fontSize: 20.sp,
     fontWeight: FontWeightHelper.bold,
   );
@@ -19,8 +19,58 @@ class TextStyles {
       color: Colors.white,
       fontSize: 16.sp,
       fontWeight: FontWeightHelper.medium);
+
   static TextStyle font13GreyRegular = TextStyle(
       color: ColorManager.grey,
       fontSize: 10.8.sp,
+      fontWeight: FontWeightHelper.regular);
+
+  static TextStyle font28BlueBold = TextStyle(
+      color: ColorManager.bule,
+      fontSize: 28.sp,
+      fontWeight: FontWeightHelper.bold);
+
+  static TextStyle font14GruyRegular = TextStyle(
+      color: ColorManager.grey,
+      fontSize: 14.sp,
+      fontWeight: FontWeightHelper.regular);
+
+  static TextStyle font14LighteRegular = TextStyle(
+      color: ColorManager.lighteGrey,
+      fontSize: 14.sp,
+      fontWeight: FontWeightHelper.regular);
+
+  static TextStyle font14DarkBlueMedium = TextStyle(
+      color: ColorManager.darkBlue,
+      fontSize: 14.sp,
+      fontWeight: FontWeightHelper.medium);
+
+  static TextStyle font12BlueRegular = TextStyle(
+      color: ColorManager.bule,
+      fontSize: 12.sp,
+      fontWeight: FontWeightHelper.regular);
+
+  static TextStyle font16WhiteSemiBold = TextStyle(
+      color: ColorManager.white,
+      fontSize: 16.sp,
+      fontWeight: FontWeightHelper.semiBold);
+
+  static TextStyle font13SpanishGrayRegular = TextStyle(
+      color: ColorManager.spanishGray,
+      fontSize: 13.sp,
+      fontWeight: FontWeightHelper.regular);
+
+  static TextStyle font13DarkBlueMedium = TextStyle(
+      color: ColorManager.darkBlue,
+      fontSize: 13.sp,
+      fontWeight: FontWeightHelper.medium);
+
+  static TextStyle font11DarkBlueRegular = TextStyle(
+      color: ColorManager.darkBlue,
+      fontSize: 11.sp,
+      fontWeight: FontWeightHelper.regular);
+  static TextStyle font11BlueRegular = TextStyle(
+      color: ColorManager.bule,
+      fontSize: 11.sp,
       fontWeight: FontWeightHelper.regular);
 }
