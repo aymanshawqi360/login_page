8eb6b66a899479b60da7c41f595c734f6fe156be
aymanshawqi360 <aymanshawqi380gmail.com>
2025-01-10 14:41:35 +0300

    - added github workflows

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -6,6 +6,7 @@ import 'package:login_app/core/routing/app_router.dart';
 import 'package:login_app/doc_app.dart';
 
 void main() async {
+  ///////////////////
   await ScreenUtil.ensureScreenSize();
   setupGetIt();
   runApp(DocApp(appRouter: AppRouter()));
